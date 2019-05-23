72ceabaaf2b769ec843b97b7f4574cc158ce59c4
emmanuel orton <yommiemanuel96@gmail.com>
2019-05-23 19:14:26 -0400

    individial assignment 1

---
+++ Tetris/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+
+[assembly: AssemblyTitle("Tetris")]
+[assembly: AssemblyDescription("Tetris game for class project")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tetris")]
+[assembly: AssemblyCopyright("2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, 
+    ResourceDictionaryLocation.SourceAssembly 
+)]
+
+
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
